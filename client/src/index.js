2e8f2d40beeae0690628e5b18e22b5efa49593e7
Nick <owennicholasbrown@gmail.com>
2020-10-23 01:16:16 -0400

    Use fantasy font family

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from 'routes.js';
+import 'global.css';
 
 ReactDOM.render(
   <Router>
